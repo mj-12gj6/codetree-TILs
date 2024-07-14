a82e59a772726e9ea62c30c0aebb141aee362b8b
mj-12gj6 <mjbori@naver.com>
2024-07-14 09:56:44 +0900

    Title: 비오는 날, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240714/비오는 날/rainy-day.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+struct data
+{
+    string date;
+    string day;
+    string weather;
+};
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    data future[100];
+
+    for(int i=0;i<n;i++)
+    {
+        cin >> future[i].date >> future[i].day >> future[i].weather;
+    }
+
+    int idx=0;
+    data rain=future[3];
+
+
+    for(int i=0;i<n;i++)
+    {
+        if(future[i].weather=="Rain")
+        {
+            
+            if(future[i].date <= rain.date)
+            {
+                rain=future[i];
+                idx=i;
+            }
+        }
+    }
+
+    cout << rain.date << " " << rain.day << " " << rain.weather;
+
+
+    return 0;
+}
