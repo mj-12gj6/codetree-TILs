89a905af7a59e17173502e1f0c60cd99c1ec2e17
mj-12gj6 <mjbori@naver.com>
2024-07-14 14:16:34 +0900

    Title: Time to Time, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240714/Time to Time/time-to-time.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+
+    cout << (c-a)*60 + d-b;
+
+
+    return 0;
+}
