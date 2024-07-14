18751a64b8c970ef9f2ff4bee342a01dae023462
mj-12gj6 <mjbori@naver.com>
2024-07-14 11:05:42 +0900

    Title: 개인정보, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240714/개인정보/personal-info.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+struct Info
+{
+    string name;
+    int h;
+    double w;
+};
+
+bool cmp_name(Info a, Info b)
+{
+    return a.name < b.name;
+}
+
+bool cmp_h(Info a,Info b)
+{
+    return a.h > b.h;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    Info per[5];
+
+    for(int i=0;i<5;i++)
+    {
+        cin >> per[i].name >> per[i].h >> per[i].w;
+    }
+
+    sort(per,per+5,cmp_name);
+
+    cout << "name\n";
+
+    for(int i=0;i<5;i++)
+    {
+        cout << per[i].name << " " << per[i].h << " " << per[i].w;
+        cout << "\n";
+    }
+
+    sort(per,per+5,cmp_h);
+
+    cout << "\nheight\n";
+
+    for(int i=0;i<5;i++)
+    {
+        cout << per[i].name << " " << per[i].h << " " << per[i].w;
+        cout << "\n";
+    }
+
+
+    return 0;
+}
