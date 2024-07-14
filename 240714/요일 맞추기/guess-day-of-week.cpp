c4a3a9a6a870845e2977bb8b3d621f1104611de9
mj-12gj6 <mjbori@naver.com>
2024-07-14 20:11:40 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240714/요일 맞추기/guess-day-of-week.cpp
+++ 240714/요일 맞추기/guess-day-of-week.cpp
@@ -5,43 +5,46 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int arr[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+    int arr[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
 
-    string day[7]={"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+    string day[7] = { "Mon","Tue","Wed","Thu","Fri","Sat","Sun" };
 
-    int m1,d1,m2,d2;
+    int m1, d1, m2, d2;
     cin >> m1 >> d1 >> m2 >> d2;
 
-    int diff=0;
+    int diff = 0;
 
 
-    if(m1>=m2)
+    if (m1 <= m2)
     {
-        for(int i=m1;i<m2;i++)
+        for (int i = m1; i < m2; i++)
         {
-            diff+=arr[i];
+            diff += arr[i];
         }
-        diff=diff+d2-d1;
+        diff = diff + d2 - d1;
     }
     else
     {
-        for(int i=m2;i<m1;i++)
+        for (int i = m2; i < m1; i++)
         {
-            diff+=arr[i];
+            diff += arr[i];
         }
-        diff=diff+d1-d2;
+        diff = diff + d1 - d2;
     }
+
 
     int n;
 
-    if(diff>0)
+
+    if (diff > 0)
     {
-        n=diff%7;
+        n = diff % 7;
     }
     else
     {
-        n=(700+diff)%7;
+        n = (700 + diff) % 7;
     }
+
     cout << day[n];
 
 
